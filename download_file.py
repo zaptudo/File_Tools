a8aedc5d15d7b408e98c317305a16113519505ab
Leandro de Araujo Loures Coelho <leandro.coelho@ibge.gov.br>
2017-06-09 16:57:55 -0300

    Initial commit.

---
+++ download_file.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+import io
+import sys
+from urllib2 import urlopen
+
+def download_length(response, output, length):
+
+    BUFF_SIZE = 1024
+
+    times = length // BUFF_SIZE
+    if length % BUFF_SIZE > 0:
+        times += 1
+
+    for time in range(times):
+        output.write(response.read(BUFF_SIZE))
+        print 'Baixados %d' % ( (float(time + 1) / float(times)) * 100 )
+
+    print 'Download concluído'
+
+
+def download(response, output):
+
+    BUFF_SIZE = 1024
+
+    total_downloaded = 0
+    while True:
+        data = response.read(BUFF_SIZE)
+        total_downloaded += len(data)
+
+        if not data:
+            break
+
+        output.write(data)
+        print('Baixados {bytes}'.format(bytes=total_downloaded))
+
+    print "Download concluído"
+
+def main():
+    response = urlopen(sys.argv[1])
+    out_file = io.FileIO('saida.zip', mode='w')
+
+    info = response.info()
+    content_length = info['Content-Length']
+
+    if content_length:
+        length = int(content_length)
+        download_length(response, out_file, length)
+    else:
+        download(response, out_file)
+
+    response.close()
+    out_file.close()
+    print('Concluído')
+
+if __name__ == '__main__':
+    main()
